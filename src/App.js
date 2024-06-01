13638e8c845c1b2541f9b820f9f5f94eff6de346
SrivastavArchit <architpulkit2003@gmail.com>
2024-06-01 23:12:41 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes,Navigate } from "react-router-dom";
+import HomePage from "./components/homePage";
+import Profile from "./components/profile";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Routes>
+      <Route path="/" element={<Navigate to="/users" />} />
+        <Route path="/users" element={<HomePage />} />
+        <Route path="/users/:userId" element={<Profile />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
